a6917b05280dd601a2a835ff9fabcc84d00b5c0f
hun2__2 <wognskec@hanyang.ac.kr>
2023-11-03 00:01:56 +0900

    [feat] State Validation
    1. State Validation
      - Property가 제대로 있는지
      - Property type이 정확한지
      - 재귀를 통해 검사

--- src/components/core/SuperComponent.js
+++ src/components/core/SuperComponent.js
@@ -1,9 +1,20 @@
+import validationState from "../utils/validation.js";
+
 export default function SuperComponent(state) {}
 
 SuperComponent.prototype.setState = function test(nextState) {
-  console.log("비교", this, this.state, nextState, this.state === nextState);
-  if (this.state === nextState) return;
-  console.log("리렌더링 함", this);
+  console.log(this, "비교", this.state, nextState);
+  // 초기값 아니고
+  if (this.state !== undefined) {
+    // 이전과 같으면 렌더링 pass
+    if (this.state === nextState) return;
+    // validation 통과못하면 에러 알림, 렌더링 pass
+    if (!validationState(this.state, nextState)) {
+      alert("nextState 에 잘못된 값이 들어왔습니다.");
+      return;
+    }
+  }
+
   this.state = nextState;
   this.render();
 };
---
+++ src/components/utils/validation.js
@@ -0,0 +1,60 @@
+const validationState = (prevState, nextState) => {
+  console.log(typeof prevState, typeof nextState);
+  if (typeof prevState !== typeof nextState) return false;
+
+  if (Array.isArray(prevState) !== Array.isArray(nextState)) return false;
+
+  // 객체 타입 중 배열인 경우
+  if (Array.isArray(prevState)) {
+    return arrayValidation(prevState, nextState);
+  }
+  // 객체 타입 중 객체인 경우
+  if (typeof prevState === "object") {
+    return objectValidation(prevState, nextState);
+  }
+
+  return true;
+};
+
+const arrayValidation = (prev, next) => {
+  return prev.every((item, index) => {
+    // next 가 더 짧으면 패스
+    if (!next[index]) return true;
+
+    // 둘다 원시
+    if (typeof item !== "object") return typeof item === typeof next[index];
+    // 하나만 배열
+    if (Array.isArray(item) !== Array.isArray(next[index])) return false;
+    // 둘 다 배열
+    if (Array.isArray(item)) return arrayValidation(item, next[index]);
+    else return objectValidation(item, next[index]);
+  });
+};
+
+const objectValidation = (prev, next) => {
+  const prevKeys = Object.keys(prev);
+  const nextKeys = Object.keys(next);
+
+  // prev 있는 것 중 next 에 없는 것이 있으면 false
+  if (!prevKeys.every((key) => nextKeys.includes(key))) return false;
+
+  // 각 키에 대해서 재귀적으로 검사
+  return prevKeys.every((key) => {
+    const prevValue = prev[key];
+    const nextValue = next[key];
+
+    // next 에 없으면 패스
+    if (!nextValue) return true;
+    // 둘 다 원시
+    if (typeof prevValue !== "object")
+      return typeof prevValue === typeof nextValue;
+    // 하나만 배열
+    if (Array.isArray(prevValue) !== Array.isArray(nextValue)) return false;
+    // 둘 다 배열
+    if (Array.isArray(prevValue)) return arrayValidation(prevValue, nextValue);
+    // 둘 다 객체
+    else return objectValidation(prevValue, nextValue);
+  });
+};
+
+export default validationState;
